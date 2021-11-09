c0bc6960d6cb3b1e78de0cc7a480d10b3fce1875
Sahibjot Singh <sahibjot366@gmail.com>
2021-11-10 03:30:20 +0530

    Second commit

--- src/index.js
+++ src/index.js
@@ -2,18 +2,31 @@ const express=require('express');
 const mongoose=require('mongoose');
 const taskRouter=require('./routers/task.js');
 const userRouter=require('./routers/user.js');
-const bcrypt=require('bcryptjs');
-const jwt=require('jsonwebtoken');
+
 
 const app=express();
 const port=process.env.PORT;
-
-mongoose.connect(process.env.MONGODB_URL,{useNewUrlParser:true});
-
-
 app.use(express.json())
 app.use(taskRouter);
 app.use(userRouter);
+
+const mongoUri=process.env.MONGODB_URL;
+// mongoose.connect(mongoUri,{useNewUrlParser:true});
+if (!mongoUri) {
+    throw new Error(
+      `MongoURI was not supplied.  Make sure you watch the video on setting up Mongo DB!`
+    );
+  }
+  mongoose.connect(mongoUri, {
+    useNewUrlParser: true
+  });
+  mongoose.connection.on("connected", () => {
+    console.log("Connected to mongo instance");
+  });
+  mongoose.connection.on("error", (err) => {
+    console.error("Error connecting to mongo", err);
+  });
+
 
 
 
